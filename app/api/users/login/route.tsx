3b66be343138d6b09abd4b3912a7b1ee0b842392
biyash <bsstha21@gmail.com>
2024-07-02 19:42:19 +0545

    auth

--- app/api/users/login/route.tsx
+++ app/api/users/login/route.tsx
@@ -10,33 +10,36 @@ export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json();
         const { email, password } = reqBody;
-        
+
         const user = await User.findOne({ email });
         if (!user) {
             return NextResponse.json({ error: "User not found" }, { status: 400 });
         }
-        
+
         console.log("user exists");
-        
+
         const validPassword = await bcrypt.compare(password, user.password);
         if (!validPassword) {
             return NextResponse.json({ message: "Check your credentials" }, { status: 400 });
         }
-        
+
         const tokenData = {
             id: user._id,
             username: user.username,
             email: user.email,
         };
-        
+
+        if (!process.env.TOKEN_SECRET) {
+            throw new Error("TOKEN_SECRET environment variable is not defined");
+        }
+
         const token = jwt.sign(tokenData, process.env.TOKEN_SECRET, { expiresIn: "1d" });
-        
+
         const response = NextResponse.json({ message: "User logged in successfully", success: true }, { status: 200 });
-        
-        response.cookies.set("token", token,{
+        response.cookies.set("token", token, {
             httpOnly: true,
-        })
-        
+        });
+
         return response;
     } catch (error: any) {
         console.log(error);
